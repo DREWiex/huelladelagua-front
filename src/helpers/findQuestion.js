c2418064d7d4671bc759dd9dddb9964023d349dd
DREWiex <aleon88@gmail.com>
2023-05-18 01:13:14 +0200

    Elimino archivos innecesarios y refact en general. Todo funcionando correctamente

--- src/helpers/findQuestion.js
+++ src/helpers/findQuestion.js
@@ -1,10 +1,10 @@
 
 /**
  * Buscar el objeto que coincida con el ID que recibe por argumento.
- * @function
+ * @function findQuestion
  * @param {Array} questions Recibe un array de objetos con los datos de las preguntas del cuestionario.
  * @param {Number} id ID del objeto del array.
- * @returns Un objeto que coincide con el ID que recibe por argumento.
+ * @returns Un objeto que coincide con el que tiene el ID que recibe por argumento.
  */
 export const findQuestion = (questions, id) => {
 
