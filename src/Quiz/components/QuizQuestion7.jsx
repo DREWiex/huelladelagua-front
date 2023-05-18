30a60c0fbc6b0f4abc5b5faffc0dfd4b33f49cf7
DREWiex <aleon88@gmail.com>
2023-05-18 17:38:17 +0200

    QQ7 done

--- src/Quiz/components/QuizQuestion7.jsx
+++ src/Quiz/components/QuizQuestion7.jsx
@@ -1,67 +1,74 @@
-import React from 'react'
-import { findQuestion } from '../../helpers'
-import { questions } from '../data/questions'
+import { findQuestion } from '../../helpers';
+import { questions } from '../data/questions';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAnswer7 } from '../../store/slices/quizSlice';
 
-export const QuizQuestion7 = ({nextPage}) => {
+export const QuizQuestion7 = ({ nextPage }) => {
 
-
-    const { answers, liters } = useSelector((state) => state.quiz)
+    const { answers } = useSelector((state) => state.quiz)
 
     const { question, description, answers: resp } = findQuestion(questions, 7);
 
     const dispatch = useDispatch();
-    
-    const handleTimes = ({target}) => {
 
-        const newLtr = target.dataset.liters
-  
-          dispatch(setAnswer7({answer:target.value, liters:newLtr}))
-          nextPage()
-      }
+    const handleTimes = ({ target }) => {
+
+        const answer = target.value; // la respuesta del usuario
+
+        const liters = Number(target.dataset.liters); // consumo medido en litros de agua/día según la respuesta de usuario
+    
+        const pixels = Number(target.dataset.pixels); // píxeles en los que aumenta o disminuye el tamaño de la ola en función de la respuesta del usuario
+    
+        dispatch(setAnswer7({ answer, liters, pixels }));
+
+        nextPage();
+
+    };
+
 
     return (
+
         <>
+
             <h2>
                 {question}
             </h2>
 
-            <p>
-                ¿Y tu lavavajillas?
-                ¿Cúantas veces se pone en marcha?
-            </p>
-
             <button
                 id='1'
-                onClick={handleTimes} 
                 value={resp[0].answer}
                 data-liters={resp[0].liters}
+                data-pixels={resp[0].pixels}
                 disabled={answers.quiz7 == '1' ? true : false}
+                onClick={handleTimes}
             >
                 {resp[0].answer}
             </button>
 
             <button
-                id='3-5' 
-                onClick={handleTimes} 
+                id='3-5'
                 value={resp[1].answer}
                 data-liters={resp[1].liters}
+                data-pixels={resp[1].pixels}
                 disabled={answers.quiz7 == '3-5' ? true : false}
+                onClick={handleTimes}
             >
                 {resp[1].answer}
             </button>
 
             <button
-                id='mano' 
-                onClick={handleTimes} 
+                id='mano'
                 value={resp[2].answer}
                 data-liters={resp[2].liters}
+                data-pixels={resp[2].pixels}
                 disabled={answers.quiz7 == 'mano' ? true : false}
+                onClick={handleTimes}
             >
                 {resp[2].answer}
             </button>
 
         </>
-    )
-}
+
+    );
+
+};
