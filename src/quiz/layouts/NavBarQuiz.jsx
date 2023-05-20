5968084c1abd6183c16908034be55d5ca8b02f05
DREWiex <aleon88@gmail.com>
2023-05-20 09:05:00 +0200

    Lógica de los divs del navbarquiz funcionando

--- src/quiz/layouts/NavBarQuiz.jsx
+++ src/quiz/layouts/NavBarQuiz.jsx
@@ -1,8 +1,10 @@
+import { useSelector } from 'react-redux';
 import { usePaginate } from '../../hooks';
 import '../styles/NavBarQuiz.css';
 
 export const NavBarQuiz = ({ page, setPage }) => {
 
+    const { answers } = useSelector(state => state.quiz);
 
     const { handlePaginate } = usePaginate(page, setPage);
 
@@ -19,18 +21,20 @@ export const NavBarQuiz = ({ page, setPage }) => {
 
                 <div className="flex">
 
+                    {/* A excepción de la primera, hasta que la pregunta anterior a la actual no esté respondida, la acción 'setPage' del evento 'onClick' no va a realizar su función */}
+
                     <div className="rectangle" onClick={() => { setPage(1) }}> {/* page 1 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(2) }}> {/* page 2 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(3) }}> {/* page 3 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(4) }}> {/* page 4 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(5) }}> {/* page 5 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(6) }}> {/* page 6 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(9) }}> {/* page 9 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(11) }}> {/* page 11 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(12) }}> {/* page 12 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(13) }}> {/* page 13 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(14) }}> {/* page 14 */} </div>
-                    <div className="rectangle" onClick={() => { setPage(15) }}> {/* page 15 */} </div>
+                    <div className="rectangle" onClick={() => { answers.quiz1 != '' && setPage(2) }}> {/* page 2 */} </div>
+                    <div className="rectangle" onClick={() => { answers.quiz2 > 0 && setPage(3) }}> {/* page 3 */} </div>
+                    <div className="rectangle" onClick={() => { answers.quiz3 != '' && setPage(4) }}> {/* page 4 */} </div>
+                    <div className="rectangle" onClick={() => { answers.quiz4 != '' && setPage(5) }}> {/* page 5 */} </div>
+                    <div className="rectangle" onClick={() => { answers.quiz5 != '' && setPage(6) }}> {/* page 6 */} </div> {/* pregunta múltiple */}
+                    <div className="rectangle" onClick={() => { answers.quiz6 != '' && setPage(9) }}> {/* page 9 */} </div> {/* pregunta múltiple */}
+                    <div className="rectangle" onClick={() => { answers.quiz9 != '' && setPage(11) }}> {/* page 11 */} </div>
+                    <div className="rectangle" onClick={() => { answers.quiz11 != '' && setPage(12) }}> {/* page 12 */} </div>
+                    <div className="rectangle" onClick={() => { answers.quiz12 != '' && setPage(13) }}> {/* page 13 */} </div>
+                    <div className="rectangle" onClick={() => { answers.quiz13 != '' && setPage(14) }}> {/* page 14 */} </div>
+                    <div className="rectangle" onClick={() => { answers.quiz14 != '' && setPage(15) }}> {/* page 15 */} </div>
 
                 </div>
 
