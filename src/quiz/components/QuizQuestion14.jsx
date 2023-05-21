9847525631862390a77d06108ab19575057f1e43
DREWiex <aleon88@gmail.com>
2023-05-21 20:26:29 +0200

    Estilos mobile QQ14 done. Pendiente 1200px

--- src/quiz/components/QuizQuestion14.jsx
+++ src/quiz/components/QuizQuestion14.jsx
@@ -6,11 +6,11 @@ import { useForm } from '../../hooks/useForm';
 
 export const QuizQuestion14 = ({ nextPage }) => {
 
-    const { form,  handleChange } = useForm();
+    const { form, handleChange } = useForm();
 
     const dispatch = useDispatch();
 
-    const { category, question, description } = findQuestion(questions, 14); // destructuración de las propiedades 'category', 'question', 'description' y 'answers' del objeto que devuelve la función
+    const { category, question, description, img } = findQuestion(questions, 14); // destructuración de las propiedades 'category', 'question', 'description', 'answers' e 'img' del objeto que devuelve la función
 
 
     const handleSubmit = (ev) => {
@@ -18,7 +18,7 @@ export const QuizQuestion14 = ({ nextPage }) => {
         ev.preventDefault();
 
         const pixelsMax = 500; // nro. máximo de píxeles para que la ola no rebase la pantalla
-        
+
         const litersMax = 11616.85; // nro. máximo de litros calculado en base a la respuesta con mayor consumo de cada pregunta (ref. media de m² para hacer el cálculo = 250)
 
         const { jardin } = form; // 'value' de la respuesta del usuario (m² del jardín)
@@ -40,29 +40,46 @@ export const QuizQuestion14 = ({ nextPage }) => {
 
         <>
 
-            <h2> {question} </h2>
+            <div className='quiz-container'>
 
-            <p> {description[0]} </p>
+                <header>
 
-            <form
-                onSubmit={handleSubmit}
-            >
+                    <h2> {question} </h2>
 
-                <input
-                    type="number"
-                    name={category}
-                    placeholder={description[1]}
-                    min='0'
-                    onChange={handleChange}
-                />
+                    <p> {description[0]} </p>
 
-                <input
-                    type="submit"
-                    value="Continuar"
-                    disabled={!form ? true : false} // no se habilita el botón continuar hasta que el usuario no introduce un valor
-                />
+                    <div>
 
-            </form>
+                        <img
+                            src={img}
+                            alt="Dos personas regando una planta sembrada junto a un árbol"
+                            title="Dos personas regando una planta sembrada junto a un árbol"
+                        />
+
+                    </div>
+
+                </header>
+
+                <form onSubmit={handleSubmit}>
+
+                    <input
+                        type="number"
+                        name={category}
+                        placeholder={description[1]}
+                        min='0'
+                        onChange={handleChange}
+                    />
+
+                    <input
+                        className="submit"
+                        type="submit"
+                        value="Continuar"
+                        disabled={!form ? true : false} // no se habilita el botón continuar hasta que el usuario no introduce un valor
+                    />
+
+                </form>
+
+            </div>
 
         </>
 
