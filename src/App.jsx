39ad4f69f1740a499556353d993d13dda59ca459
DREWiex <aleon88@gmail.com>
2023-05-21 08:59:28 +0200

    Creo carpeta styles dentro de src y divido archivos css para limpiar el index.css. Todo funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { AppRouter } from "./routers/AppRouter";
 function App() {
 
   const { answers, liters, pixels } = useSelector(state => state.quiz);
+
+  //console.log(answers)
   
 
   return (
