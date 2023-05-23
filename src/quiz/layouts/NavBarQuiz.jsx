a4392091e6c10307d9260c1da9339a281f1a8e2f
DREWiex <aleon88@gmail.com>
2023-05-23 09:26:48 +0200

    Corrección reducer answers.quiz2 y rectangles del navbarquiz

--- src/quiz/layouts/NavBarQuiz.jsx
+++ src/quiz/layouts/NavBarQuiz.jsx
@@ -53,16 +53,16 @@ export const NavBarQuiz = ({ page, setPage }) => {
 
                         <div className="rectangle" onClick={() => { setPage(1) }}> {/* page 1 */} </div>
                         <div className="rectangle" onClick={() => { answers.quiz1 && setPage(2) }}> {/* page 2 */} </div>
-                        <div className="rectangle" onClick={() => { answers.quiz2 && setPage(3) }}> {/* page 3 */} </div>
+                        <div className="rectangle" onClick={() => { answers.quiz1 && setPage(3) }}> {/* page 3 */} </div> {/* Como 'answers.quiz2' siempre es 'true', ya que se inicializa en 1 y puede avanzar sin modificar la respuesta, no permitimos que avance hasta la page 3 hasta que la pregunta 1 haya sido respondida */}
                         <div className="rectangle" onClick={() => { answers.quiz3 && setPage(4) }}> {/* page 4 */} </div>
                         <div className="rectangle" onClick={() => { answers.quiz4 && setPage(5) }}> {/* page 5 */} </div>
                         <div className="rectangle" onClick={() => { answers.quiz5 && setPage(6) }}> {/* page 6 */} </div> {/* pregunta múltiple */}
-                        <div className="rectangle" onClick={() => { answers.quiz6 && setPage(9) }}> {/* page 9 */} </div> {/* pregunta múltiple */}
-                        <div className="rectangle" onClick={() => { answers.quiz9 && setPage(11) }}> {/* page 11 */} </div>
-                        <div className="rectangle" onClick={() => { answers.quiz11 && setPage(12) }}> {/* page 12 */} </div>
-                        <div className="rectangle" onClick={() => { answers.quiz12 && setPage(13) }}> {/* page 13 */} </div>
-                        <div className="rectangle" onClick={() => { answers.quiz13 && setPage(14) }}> {/* page 14 */} </div>
-                        <div className="rectangle" onClick={() => { answers.quiz14 && setPage(15) }}> {/* page 15 */} </div>
+                        <div className="rectangle" onClick={() => { answers.quiz6 && setPage(9) }}> {/* page 7 */} </div> {/* pregunta múltiple */}
+                        <div className="rectangle" onClick={() => { answers.quiz9 && setPage(11) }}> {/* page 8 */} </div>
+                        <div className="rectangle" onClick={() => { answers.quiz11 && setPage(12) }}> {/* page 9 */} </div>
+                        <div className="rectangle" onClick={() => { answers.quiz12 && setPage(13) }}> {/* page 10 */} </div>
+                        <div className="rectangle" onClick={() => { answers.quiz13 && setPage(14) }}> {/* page 11 */} </div>
+                        <div className="rectangle" onClick={() => { answers.quiz14 && setPage(15) }}> {/* page 12 */} </div>
 
                     </div>
 
--- src/store/slices/quizSlice.js
+++ src/store/slices/quizSlice.js
@@ -8,7 +8,7 @@ export const quizSlice = createSlice({
     initialState: {
         answers: {
             quiz1: undefined,
-            quiz2: 0,
+            quiz2: 1,
             quiz3: undefined,
             quiz4: undefined,
             quiz5: undefined,
@@ -89,7 +89,7 @@ export const quizSlice = createSlice({
 
                 case 'restar':
                     state.count.length > 1 ? state.count.pop() : null;
-                    state.answers.quiz2 = state.answers.quiz2 - 1;
+                    state.answers.quiz2 > 1 ? state.answers.quiz2 = state.answers.quiz2 - 1 : null;
                     break;
             };
 
